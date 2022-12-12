f01145cd8aa1358c0110fa65795fbdc35f742205
unknown <baarminas@gmail.com>
2022-12-12 17:56:51 +0200

    skundu forma up

--- scripts.js
+++ scripts.js
@@ -1,81 +1,31 @@
-const HELPBTN = document.querySelector("#help-btn");
-HELPBTN.addEventListener("click", openComplaintForm);
-const CLOSEBTN = document.querySelector("#close-btn");
-CLOSEBTN.addEventListener("click", closeComplaintForm);
-const SUBMITBTN = document.querySelector("#submit-btn");
-SUBMITBTN.addEventListener("click", submitForm);
+const helpButton = document.querySelector("#help-btn");
+helpButton.addEventListener("click", openComplaintForm);
+const closeButton = document.querySelector("#close-btn");
+closeButton.addEventListener("click", closeComplaintForm);
+const submitButton = document.querySelector("#submit-btn");
+submitButton.addEventListener("click", submitForm);
 
 
-const NAMEINPUT = document.querySelector("#name");
-const SURNAMEINPUT = document.querySelector("#surname");
-const EMAILINPUT = document.querySelector("#email");
-const COMPLAINT = document.querySelector("#complaint");
+const nameInput = document.querySelector("#name");
+const surnameInput = document.querySelector("#surname");
+const emailInput = document.querySelector("#email");
+const complaint = document.querySelector("#complaint");
 
-const RECEIVEDNAME = document.querySelector("#received-name");
-const RECEIVEDSURNAME = document.querySelector("#received-surname");
-const RECEIVEDEMAIL = document.querySelector("#received-email");
-const RECEIVEDCOMPLAINT = document.querySelector("#received-complaint");
-const COMPLAINTDATE = document.querySelector("#complaint-date");
-const ERRORMSG = document.querySelector("#error");
+const receivedName = document.querySelector("#received-name");
+const receivedSurname = document.querySelector("#received-surname");
+const receivedEmail = document.querySelector("#received-email");
+const receivedComplaint = document.querySelector("#received-complaint");
+const complaintDate = document.querySelector("#complaint-date");
+const errorMessage = document.querySelector("#error");
 
-const COMPLAINTFORM = document.querySelector("#form-container");
+const complaintForm = document.querySelector("#form-container");
 
 function openComplaintForm() {
-    // COMPLAINTFORM.style.display = "flex";
-    COMPLAINTFORM.style.visibility = "visible";
-    COMPLAINTFORM.style.opacity = "1";
+    complaintForm.classList.add("active");
 }
 
-function closeComplaintForm() {
-    // COMPLAINTFORM.style.display = "none";
-    COMPLAINTFORM.style.visibility = "hidden";
-    COMPLAINTFORM.style.opacity = "0";
-}
-
-function submitForm() {
-    const ISNAMEVALID = NAMEINPUT.value;
-    const ISSURNAMEVALID = SURNAMEINPUT.value;
-    const ISEMAILVALID = isEmailValid();
-    const ISCOMPLAINTVALID = COMPLAINT.value;
-
-    if (ISNAMEVALID &&
-        ISSURNAMEVALID &&
-        ISEMAILVALID &&
-        ISCOMPLAINTVALID) {
-        removeError();
-        RECEIVEDNAME.innerHTML = NAMEINPUT.value;
-        RECEIVEDSURNAME.innerHTML = SURNAMEINPUT.value;
-        RECEIVEDEMAIL.innerHTML = EMAILINPUT.value;
-        RECEIVEDCOMPLAINT.innerHTML = COMPLAINT.value;
-        COMPLAINTDATE.innerHTML = new Date().toLocaleString();
-        clearInput();
-        closeComplaintForm()
-    } else {
-        removeError()
-        if (!ISNAMEVALID) {
-            NAMEINPUT.style.border = "1px solid red";
-            addError();
-        }
-
-        if (!ISSURNAMEVALID) {
-            SURNAMEINPUT.style.border = "1px solid red";
-            addError();
-        }
-
-        if (!ISEMAILVALID || !EMAILINPUT.value) {
-            EMAILINPUT.style.border = "1px solid red";
-            addError();
-        }
-
-        if (!ISCOMPLAINTVALID) {
-            COMPLAINT.style.border = "1px solid red";
-            addError();
-        }
-    }
-}
-
-function isEmailValid() {
-    if (EMAILINPUT.validity.typeMismatch) {
+function emailIsValid() {
+    if (emailInput.validity.typeMismatch) {
         return false;
     } else {
         return true
@@ -83,20 +33,69 @@ function isEmailValid() {
 }
 
 function clearInput() {
-    NAMEINPUT.value = "";
-    SURNAMEINPUT.value = "";
-    EMAILINPUT.value = "";
-    COMPLAINT.value = "";
+    nameInput.value = "";
+    surnameInput.value = "";
+    emailInput.value = "";
+    complaint.value = "";
 }
 
 function addError() {
-    ERRORMSG.innerHTML = "Please corect mistakes made in your complaint form.";
+    errorMessage.innerHTML = "Please corect mistakes made in your complaint form.";
 }
 
 function removeError() {
-    ERRORMSG.innerHTML = "";
-    NAMEINPUT.style.border = "1px solid black";
-    SURNAMEINPUT.style.border = "1px solid black";
-    EMAILINPUT.style.border = "1px solid black";
-    COMPLAINT.style.border = "1px solid black";
+    errorMessage.innerHTML = "";
+    nameInput.style.border = "1px solid black";
+    surnameInput.style.border = "1px solid black";
+    emailInput.style.border = "1px solid black";
+    complaint.style.border = "1px solid black";
 }
+
+function closeComplaintForm() {
+    complaintForm.classList.remove("active");
+}
+
+function submitForm() {
+    const isNameValid = nameInput.value;
+    const isSurnameValid = surnameInput.value;
+    const isEmailValid = emailIsValid();
+    const isComplaintValid = complaint.value;
+
+
+    if (isNameValid &&
+        isSurnameValid &&
+        isEmailValid &&
+        isComplaintValid) {
+        removeError();
+        receivedName.innerHTML = nameInput.value;
+        receivedSurname.innerHTML = surnameInput.value;
+        receivedEmail.innerHTML = emailInput.value;
+        receivedComplaint.innerHTML = complaint.value;
+        complaintDate.innerHTML = new Date().toLocaleString();
+        clearInput();
+        closeComplaintForm()
+    } else {
+        console.log("ne");
+        removeError()
+        if (!isNameValid) {
+            nameInput.style.border = "1px solid red";
+            addError();
+        }
+
+        if (!isSurnameValid) {
+            surnameInput.style.border = "1px solid red";
+            addError();
+        }
+
+        if (!isEmailValid || !emailInput.value) {
+            emailInput.style.border = "1px solid red";
+            addError();
+        }
+
+        if (!isComplaintValid) {
+            complaint.style.border = "1px solid red";
+            addError();
+        }
+    }
+}
+
