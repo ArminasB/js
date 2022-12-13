657878956de3cbb02024669b7c80f4ec0d703aae
unknown <baarminas@gmail.com>
2022-12-13 18:04:00 +0200

    input form updated

--- scripts.js
+++ scripts.js
@@ -1,64 +1,108 @@
-// const selectElement = document.querySelector("#ice-cream");
-// selectElement.addEventListener("change", selectListener);
+const helpButton = document.querySelector("#help-btn");
+const closeButton = document.querySelector("#close-btn");
+const submitButton = document.querySelector("#submit-btn");
+const nameInput = document.querySelector("#name");
+const surnameInput = document.querySelector("#surname");
+const emailInput = document.querySelector("#email");
+const complaint = document.querySelector("#complaint");
+const receivedName = document.querySelector("#received-name");
+const receivedSurname = document.querySelector("#received-surname");
+const receivedEmail = document.querySelector("#received-email");
+const receivedComplaint = document.querySelector("#received-complaint");
+const complaintDate = document.querySelector("#complaint-date");
+const errorMessage = document.querySelector("#error");
+const complaintForm = document.querySelector("#form-container");
 
-// function selectListener(event) {
-//     console.log("event" , event);
-//     console.log("event.target", event.targer);
-//     console.log("event.target.value", event.target.value);
-//     console.log("selectElement", selectElement);
-// }
+helpButton.addEventListener("click", openComplaintForm);
+closeButton.addEventListener("click", closeComplaintForm);
+submitButton.addEventListener("click", submitForm);
 
-// window.addEventListener("resize", function(evt) {
-//     console.log("resize");
-// });
-
-// const changeLogEl = document.querySelector("#change-log");
-// const genderEl = document.querySelector("#gender");
-// const ageEl = document.querySelector("#age");
-
-// function changeListener(event) {
-//     const selectName = event.target.name;
-//     const selectValue = event.target.value;
-
-//     const text = `User has changed ${selectName} select with a value ${selectValue}`
-
-//     changeLogEl.innerText = text;
-// }
-
-// ageEl.addEventListener("change", changeListener);
-// genderEl.addEventListener("change", changeListener);
-
-const firstNumber = document.querySelector("#first");
-const secondNumber = document.querySelector("#second");
-
-const multiplyButton = document.querySelector("#multiply");
-const divideButton = document.querySelector("#divide");
-
-const resultEl = document.querySelector("#result");
-
-multiplyButton.addEventListener("click", function() {
-    const firstValue = getElementValue(firstNumber);
-    const secondValue = getElementValue(secondNumber);
-    const resultValue = multiply(firstValue, secondValue);
-    return resultEl.innerHTML = resultValue;
-});
-
-divideButton.addEventListener("click", function() {
-    const firstValue = getElementValue(firstNumber);
-    const secondValue = getElementValue(secondNumber);
-    const resultValue = divide(firstValue, secondValue);
-    return resultEl.innerHTML = resultValue;
-});
-
-function getElementValue(element) {
-    const elementValue = Number(element.value);
-    return elementValue;
+function openComplaintForm() {
+    complaintForm.classList.add("active");
 }
 
-function multiply(a, b) {
-    return a * b;
+function emailIsValid() {
+    if (emailInput.validity.typeMismatch) {
+        return false;
+    } else {
+        return true
+    }
 }
 
-function divide(a, b) {
-    return a / b;
+function clearInput() {
+    nameInput.value = "";
+    surnameInput.value = "";
+    emailInput.value = "";
+    complaint.value = "";
+}
+
+function placeData() {
+    receivedName.innerHTML = nameInput.value;
+    receivedSurname.innerHTML = surnameInput.value;
+    receivedEmail.innerHTML = emailInput.value;
+    receivedComplaint.innerHTML = complaint.value;
+    complaintDate.innerHTML = new Date().toLocaleString();
+}
+
+function addError() {
+    errorMessage.innerHTML = "Please corect mistakes made in your complaint form.";
+}
+
+function addErrorBorder(a) {
+    a.style.border = "1px solid red";
+}
+
+function addNormalBorder(a) {
+    a.style.border = "1px solid black";
+}
+
+function removeError() {
+    errorMessage.innerHTML = "";
+    nameInput.style.border = addNormalBorder(nameInput);
+    surnameInput.style.border = addNormalBorder(surnameInput);
+    emailInput.style.border = addNormalBorder(emailInput);
+    complaint.style.border = addNormalBorder(complaint);
+}
+
+function closeComplaintForm() {
+    complaintForm.classList.remove("active");
+}
+
+function submitForm() {
+    const isNameValid = nameInput.value;
+    const isSurnameValid = surnameInput.value;
+    const isEmailValid = emailIsValid();
+    const isComplaintValid = complaint.value;
+
+
+    if (isNameValid &&
+        isSurnameValid &&
+        isEmailValid &&
+        isComplaintValid) {
+        removeError();
+        placeData();
+        clearInput();
+        setTimeout(closeComplaintForm, 200);
+    } else {
+        removeError()
+        if (!isNameValid) {
+            nameInput.style.border = addErrorBorder(nameInput);
+            addError();
+        }
+
+        if (!isSurnameValid) {
+            surnameInput.style.border = addErrorBorder(surnameInput);
+            addError();
+        }
+
+        if (!isEmailValid || !emailInput.value) {
+            emailInput.style.border = addErrorBorder(emailInput);
+            addError();
+        }
+
+        if (!isComplaintValid) {
+            complaint.style.border = addErrorBorder(complaint);
+            addError();
+        }
+    }
 }
